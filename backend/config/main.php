494279c19c81ce5c042df9c3c8f754267fc34930
vin-ira <nivari@yandex.ru>
2023-09-20 11:21:15 +0300

    [new]: Generate models and CRUD

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,7 +7,9 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-backend',
+    'id' => 'apples',
+    'name' => 'Яблоки',
+    'language' => 'ru',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
@@ -37,14 +39,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+//            'rules' => [
+//                'apples' => 'apples/index'
+//            ],
         ],
-        */
     ],
     'params' => $params,
 ];
